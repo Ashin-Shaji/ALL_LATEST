7a8c893c4a49eb6815a35425588d01bc34718f88
Ashin-Shaji <122869032+Ashin-Shaji@users.noreply.github.com>
2023-11-29 20:58:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ st.set_page_config(
         'About': 'This is a header. This is an extremely cool app!'
     }
 )
-image = Image.open("howe (1).png")
+image = Image.open("log.png")
 st.image(image, use_column_width=50)
 
 st.markdown("<h1 style='text-align: center; color: Blue'>JD & RESUME MATCHING MATRIX </h1>",
