0de9e5968f20c6ae90fd0946a82cba0853b4d44c
Ashin-Shaji <122869032+Ashin-Shaji@users.noreply.github.com>
2023-11-29 14:53:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -236,7 +236,9 @@ else:
 
         final_data['Experience_Tag'] = final_data[['JD_Experience', 'Experience']].apply(
             lambda x: 1 if x['Experience'] >= x['JD_Experience'] else 0, axis=1)
-
+        
+        # final_data['Matching_Score'] = final_data[['Skill_Similarity', 'Experience_Tag']].apply(lambda x: (x['Skill_Similarity']+x['Experience_Tag'])/2, axis=1)
+       
         # final_data['Matching_Score'] = final_data[['Skill_Similarity', 'Experience_Tag']].apply(
         #     lambda x: (2 * x['Skill_Similarity'] + x['Experience_Tag']) / (2 + 1) if x['Skill_Similarity'] > 0 else 0,axis=1)
 
