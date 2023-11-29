53ee8fb486be662c97f00037e76e0473e1817670
Ashin-Shaji <122869032+Ashin-Shaji@users.noreply.github.com>
2023-11-29 21:18:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,8 +145,8 @@ st.set_page_config(
 # image = Image.open("log.png")
 # st.image(image, use_column_width=50)
 
-image = Image.open("ho.png").resize((100, 86))
-st.image(image)
+image = Image.open("ho.png"))
+st.image(image,use_column_width=50)
 
 st.markdown("<h1 style='text-align: center; color: Blue'>JD & RESUME MATCHING MATRIX </h1>",
             unsafe_allow_html=True)
